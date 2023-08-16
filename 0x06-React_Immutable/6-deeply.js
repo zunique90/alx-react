e5f684f3ffa278653ed64792da7449b7a57ef5b4
charles ezebuike <ezebuike.charles0@gmail.com>
2023-08-16 22:47:45 +0100

    task 6: Nested Merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,4 +1,5 @@
-import { Map } from "immutable";
+import { Map } from 'immutable';
 
-export const mergeDeeplyElements = (page1, page2) =>
-  Map(page1).mergeDeep(Map(page2));
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
+}
